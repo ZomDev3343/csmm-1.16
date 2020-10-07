739b72903a119c92ff9d3419950adfaaad3d20a7
Zom <contact.zomzd@gmail.com>
2020-10-07 12:24:24 +0200

    Episode 5 : Les Outils

---
+++ src/main/java/fr/zom/csmm/utils/CustomItemTiers.java
@@ -0,0 +1,56 @@
+package fr.zom.csmm.utils;
+
+import fr.zom.csmm.init.ModItems;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+public enum CustomItemTiers implements IItemTier {
+    COPPER(1, 420, 4.8f, 1.5f, 10, new LazyValue<>(() -> {return Ingredient.fromItems(ModItems.COPPER_INGOT.get());}));
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    CustomItemTiers(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, LazyValue<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.getValue();
+    }
+}
