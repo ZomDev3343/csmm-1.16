81ded8d527e2e6ecb6d38ece05d2aab3e461ca34
Zom <contact.zomzd@gmail.com>
2021-02-05 11:47:34 +0100

    - Episode Nourriture

--- src/main/java/fr/zom/csmm/init/ModItems.java
+++ src/main/java/fr/zom/csmm/init/ModItems.java
@@ -6,12 +6,13 @@ import fr.zom.csmm.utils.CustomItemTiers;
 import fr.zom.csmm.utils.ModItemGroups;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class ModItems
-{
+public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CSMM.MODID);
 
@@ -29,5 +30,19 @@ public class ModItems
     public static final RegistryObject<Item> AMETHYST_CHESTPLATE = ITEMS.register("amethyst_chestplate", () -> new ArmorItem(CustomArmorMaterials.AMETHYST_ARMOR, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)));
     public static final RegistryObject<Item> AMETHYST_LEGGINGS = ITEMS.register("amethyst_leggings", () -> new ArmorItem(CustomArmorMaterials.AMETHYST_ARMOR, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)));
     public static final RegistryObject<Item> AMETHYST_BOOTS = ITEMS.register("amethyst_boots", () -> new ArmorItem(CustomArmorMaterials.AMETHYST_ARMOR, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)));
+
+    public static final RegistryObject<Item> BLUE_APPLE = ITEMS.register("blue_apple", () -> new Item(new Item.Properties()
+            .group(ModItemGroups.CSMM_TAB)
+            .food(new Food.Builder().hunger(7).saturation(0.8F).effect(() -> new EffectInstance(Effects.SPEED, 20 * 30, 0), 1.0f).build())));
+
+    public static final RegistryObject<Item> MYRTLE_JUICE = ITEMS.register("myrtle_juice", () -> new Item(new Item.Properties()
+            .group(ModItemGroups.CSMM_TAB)
+            .food(new Food.Builder().hunger(3).saturation(0.2F).effect(() -> new EffectInstance(Effects.STRENGTH, 20 * 15, 1), 0.5f).build()))
+    {
+        @Override
+        public UseAction getUseAction(ItemStack stack) {
+            return UseAction.DRINK;
+        }
+    });
 
 }
